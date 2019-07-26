fbdb95d45d932de8c51604ccb61228cc8d669395
kendell <kendell.r@outlook.com>
2019-07-26 15:17:25 -0700

    Delete script.js

--- cwk/gocallme/script.js
+++
@@ -1,7 +0,0 @@
-function call() {
-  var telLink = document.createElement("a");
-  telLink.href = "tel:+1-562-867-5309"
-  telLink.id = "telly"
-  document.body.appendChild(telLink)
-  document.getElementById("telly").click();
-}
