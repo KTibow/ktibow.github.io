8c237143f3dae63cf0ad6dab0bd1ea91758f908c
kendell <KTibow@users.noreply.github.com>
2019-10-19 08:39:46 -0700

    Update generate_sitemap.sh

--- generate_sitemap.sh
+++ generate_sitemap.sh
@@ -2,7 +2,8 @@ contents=`cat sitemap.txt`
 echo "https://ktibow.github.io/"
 for line in $contents;
 do
-    if [[ -d ".$line" ]]; then
+    $linetwo = ".$line"
+    if [[ -d $linetwo ]]; then
         echo "https://ktibow.github.io$line/"
     fi
 done
