382ff065e565879c09973f727c4d48fc52d647ef
kendell <KTibow@users.noreply.github.com>
2019-10-20 16:22:16 -0700

    Update up-to-date.sh

--- up-to-date.sh
+++ up-to-date.sh
@@ -1,7 +1,7 @@
 find . | grep -v ".*/\..*" > page-things.txt
 contents=`cat page-things.txt`
-$contents=awk 'BEGIN{FIELDWIDTHS="1 1000"}{print $2}' $contents
+$contents=`awk 'BEGIN{FIELDWIDTHS="1 1000"}{print $2}' $contents`
 for line in $contents;
 do
-  echo "https://ktibow.github.io{$line}"
+  echo "https://ktibow.github.io$line"
 done
