7d57aee6c1c4606ddd2d62a536e9f107a107c7dd
KTibow <KTibow@users.noreply.github.com>
2019-10-22 14:28:47 +0000

    Change Lighthouse page

--- actions/generate_sitemap.sh
+++
@@ -1,9 +0,0 @@
-contents=`cat old_sitemap.txt`
-echo "https://ktibow.github.io/"
-for line in $contents;
-do
-    linetwo=".${line}/"
-    if [ -d $linetwo ]; then
-        echo "https://ktibow.github.io$line/"
-    fi
-done
