eaf1f12f2fbb9c1ba94eb50164a8baefee7ad235
kendell <KTibow@users.noreply.github.com>
2019-10-22 07:46:11 -0700

    Revert "Change Lighthouse page"
    This reverts commit 7d57aee6c1c4606ddd2d62a536e9f107a107c7dd.
    
    Adds old files back

---
+++ actions/generate_sitemap.sh
@@ -0,0 +1,9 @@
+contents=`cat old_sitemap.txt`
+echo "https://ktibow.github.io/"
+for line in $contents;
+do
+    linetwo=".${line}/"
+    if [ -d $linetwo ]; then
+        echo "https://ktibow.github.io$line/"
+    fi
+done
