110dee1bf1a2ad2aa247ced589feae69e8470313
kendell <KTibow@users.noreply.github.com>
2019-10-24 06:41:31 -0700

    Update lighthouse_report.sh

--- lighthouse_report.sh
+++ lighthouse_report.sh
@@ -2,7 +2,7 @@ rm -r lighthouse || true
 mkdir lighthouse
 cat sitemap.txt | while read line
 do 
-  report="./${line////-}.html"
+  report="${line////-}.html"
   report="${string:8:${#string}-1}"
   report="./lighthouse/$report"
   lighthouse --chrome-flags="--headless" --output-path=$report $line
