fe423af4526d1360bdedd806f0b29ee5d99aa5a8
kendell <KTibow@users.noreply.github.com>
2019-10-25 07:14:51 -0700

    Update lighthouse_report.sh

--- lighthouse_report.sh
+++ lighthouse_report.sh
@@ -2,6 +2,6 @@ rm -r lighthouse || true
 mkdir lighthouse
 cat sitemap.txt | while read line
 do 
-  filename=date +%s
+  filename=`date "+%s"`
   lighthouse --chrome-flags="--headless" --output=json --output-path="./${filename}.json" $line
 done
