c46ca22167d583d47760349d32dc9fc1a97a66ff
kendell <KTibow@users.noreply.github.com>
2019-10-25 06:54:56 -0700

    Update lighthouse_report.sh

--- lighthouse_report.sh
+++ lighthouse_report.sh
@@ -2,5 +2,5 @@ rm -r lighthouse || true
 mkdir lighthouse
 cat sitemap.txt | while read line
 do 
-  lighthouse --chrome-flags="--headless" --output json $line
+  lighthouse --chrome-flags="--headless" $line --output json
 done
