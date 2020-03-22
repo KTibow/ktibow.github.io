5d5c15b55b4f0a7f0502a32936fea232c7c40759
kendell <KTibow@users.noreply.github.com>
2020-03-22 11:13:41 -0700

    Update sitemapxml.py

--- sitemapxml.py
+++ sitemapxml.py
@@ -3,13 +3,18 @@ out.write("""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">""")
 input = open("sitemap.txt", "r").read()
 for url in input.split("\n"):
-  out.write("""<url>
+  if "https://ktibow.github.io/" in url:
+    out.write("""<url>
 <loc>"""+url+"""</loc>
 <priority>""")
-  priority = 0.5
-  if url in ["https://ktibow.github.io/", "https://ktibow.github.io/blog/", "https://ktibow.github.io/tutorials/quick-pi/", "https://ktibow.github.io/blog/2020/3/"]:
-    priority = 0.8
-  out.write("0."+str(round(priority*100))+"""</priority>
+    priority = 0.5
+    if url in ["https://ktibow.github.io/", "https://ktibow.github.io/blog/", "https://ktibow.github.io/tutorials/quick-pi/", "https://ktibow.github.io/blog/2020/3/"]:
+      priority = 0.75
+    if url in ["https://ktibow.github.io/"]:
+      priority = 1
+    if "blog" in url and url.count("/") < 7 and url.count("/") > 4:
+      priority = 0.25
+    out.write("0."+str(round(priority*100))+"""</priority>
 </url>""")
-out.write("</urlset")
+out.write("</urlset>")
 out.close()
