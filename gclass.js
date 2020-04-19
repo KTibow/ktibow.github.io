dde133fdf6832987670898f652705dcb01ca29e2
kendell <KTibow@users.noreply.github.com>
2020-04-19 09:37:56 -0700

    Update gclass.js

--- gclass.js
+++ gclass.js
@@ -9,6 +9,12 @@ class ClassroomShare extends HTMLElement {
 	connectedCallback() {
 		var currentPage = window.location;
 		this.innerHTML = "<a href=\"https://accounts.google.com/SignOutOptions?hl=en&amp;continue=https://classroom.google.com/u/0/share%3Furl"+currentPage+"\" style=\"text-decoration: none; display: inline-block; margin: 10px 5px 10px 24px; position: relative;\"><img src=\"https://ktibow.github.io/classroom-logo.png\" style=\"position: absolute; border-radius: 50%; height: 32px; top: -6px; left: -18px;\"> <span style=\"padding: 3px 6px 3px 22px; background-color: green; border-radius: 3px; color: white; font-family: Open Sans;\">Share to Classroom</span></a>";
+		this.onmouseenter = function() {
+			this.style.filter = "brightness(90%)";
+		}
+		this.onmouseleave = function() {
+			this.style.filter = "brightness(100%)";
+		}
 	}
 }
 function start() {
