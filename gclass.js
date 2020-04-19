1a4a4d025e8b4e6e4b560d91ce5c6ea22281fe52
kendell <KTibow@users.noreply.github.com>
2020-04-19 08:28:06 -0700

    Update gclass.js

--- gclass.js
+++ gclass.js
@@ -3,42 +3,17 @@ newScript.type = 'text/javascript';
 newScript.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
 document.getElementsByTagName('head')[0].appendChild(newScript);
 WebFont.load({
-		google: {
+	google: {
 			families: ['Open Sans']
 		}
 	});
-class WordCount extends HTMLLinkElement {
-  constructor() {
-    // Always call super first in constructor
-    super();
-
-    // count words in element's parent element
-    var wcParent = this.parentNode;
-
-    function countWords(node){
-      var text = node.innerText || node.textContent
-      return text.split(/\s+/g).length;
-    }
-
-    var count = 'Words: ' + countWords(wcParent);
-
-    // Create a shadow root
-    var shadow = this.attachShadow({mode: 'open'});
-
-    // Create text node and add word count to it
-    var text = document.createElement('span');
-    text.textContent = count;
-
-    // Append it to the shadow root
-    shadow.appendChild(text);
-
-
-    // Update count when element content changes
-    setInterval(function() {
-      var count = 'Words: ' + countWords(wcParent);
-      text.textContent = count;
-    }, 200)
-
+class ClassroomShare extends HTMLDivElement {
+	constructor() {
+		// Always call super first in constructor
+		super();
+		var currentPage = window.location;
+		this.style = "filter: brightness(90%);";
+		this.innerHTML = "<a href=\"https://accounts.google.com/SignOutOptions?hl=en&amp;continue=https://classroom.google.com/u/0/share%3Furl"+currentPage+"\" style=\"text-decoration: none; display: inline-block; margin: 10px 5px 10px 24px; position: relative;\"><img src=\"https://ktibow.github.io/classroom-logo.png\" style=\"position: absolute; border-radius: 50%; height: 32px; top: -6px; left: -18px;\"> <span style=\"padding: 3px 6px 3px 22px; background-color: green; border-radius: 3px; color: white; font-family: Open Sans;\">Share to Classroom</span></a>"
   }
 }
-customElements.define("gclass-share", ClassroomShare, { extends: 'a' });
+customElements.define("gclass-share", ClassroomShare, { extends: 'div' });
