ccbf8c0723a06f53a96d2a75fb36bc234855c688
kendell <KTibow@users.noreply.github.com>
2020-04-20 09:20:22 -0700

    Update gclass.js

--- gclass.js
+++ gclass.js
@@ -9,11 +9,10 @@ class ClassroomShare extends HTMLElement {
 	connectedCallback() {
 		var currentPage = window.location;
 		var myAttributes = Array.from(this.attributes);
-		var attr;
-		for (attr in myAttributes) {
-			console.log(attr);
-		}
 		var mySharePage = "https://accounts.google.com/AccountChooser?continue=https%3A%2F%2Fclassroom.google.com%2Fshare%3Furl%3D"+currentPage;
+		for (attr in myAttributes) {
+			console.log(myAttributes[attr]);
+		}
 		this.innerHTML = `<a href="${mySharePage}" style="text-decoration: none; display: inline-block; margin: 17px 5px 10px 24px; position: relative;">
 <img src="https://ktibow.github.io/classroom-logo.png" style="position: absolute; border-radius: 50%; height: 32px; top: -6px; left: -18px;">
 <span style="padding: 3px 6px 3px 22px; background-color: green; border-radius: 3px; color: white; font-family: Open Sans;">Share to Classroom</span>
