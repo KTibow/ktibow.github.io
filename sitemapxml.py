db8f8f78868186afe4a18b7c3a35e1b2db2d2717
kendell <KTibow@users.noreply.github.com>
2020-05-29 11:07:02 -0700

    Update sitemapxml.py

--- sitemapxml.py
+++ sitemapxml.py
@@ -1,9 +1,12 @@
 robots = open("robots.txt", "r").read()
+print(robots)
 out = open("sitemap.xml", "w")
 out.write("""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">""")
 input = open("sitemap.txt", "r").read()
 for url in input.split("\n"):
+  print("Parsing url "+url)
+  print(url in robots)
   if "https://ktibow.github.io/" in url and url not in robots:
     out.write("""<url>
 <loc>"""+url+"""</loc>
