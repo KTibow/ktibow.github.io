772da5d3cc462f2619618b276e2afad6cdec2ccb
kendell <KTibow@users.noreply.github.com>
2020-06-10 09:29:34 -0700

    Create phoneloader.js

---
+++ analyze/phoneloader.js
@@ -0,0 +1,1 @@
+(function(){var a=document.createElement("script");a.async=1;a.src="//www.gstatic.com/call-tracking/call-tracking_5.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b);}).call(this);
