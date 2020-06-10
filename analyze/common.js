db37dc81551196fdb710f3e1034b21dd897d4c13
kendell <KTibow@users.noreply.github.com>
2020-06-10 07:32:33 -0700

    Create common.js

---
+++ analyze/common.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-165004437-1');
+var anny = document.createElement("script");
+anny.async = true;
+anny.src = "https://ktibow.github.io/ana.js";
+document.body.appendChild(anny);
