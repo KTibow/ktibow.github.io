9c566d80924b7bec57ab402fb6b5ba7e20793c37
kendell <KTibow@users.noreply.github.com>
2020-06-10 19:05:45 -0700

    Update common.js

--- common.js
+++ common.js
@@ -3,12 +3,27 @@ function gtag() {
   dataLayer.push(arguments);
 }
 gtag('js', new Date());
-gtag('config', 'UA-165004437-1');
+gtag('config', 'UA-165' + '004437-1', { 'anonymize_ip': true });
 var anny = document.createElement("script");
 anny.async = true;
 anny.src = "https://ktibow.github.io/analyze/ana.js";
 document.body.appendChild(anny);
-var anny = document.createElement("script");
-anny.async = true;
-anny.src = "https://ktibow.github.io/gclass.js";
-document.body.appendChild(anny);
+var gcls = document.createElement("script");
+gcls.async = true;
+gcls.src = "https://ktibow.github.io/gclass.js";
+document.body.appendChild(gcls);
+function trackClick(event) {
+  event.preventDefault();
+  gtag("event", "clicked_on_"+window.location.href, {
+    "event_callback": function() {
+      this.myelem.click();
+    }
+  });
+}
+function addClickTracker() {
+  var atags = document.getElementsByTagName("a");
+  for (var i = 0; i < atags.length; i++) {
+    atags[i].addEventListener("click", trackClick.bind({myelem: atags[i]}));
+  }
+}
+window.addEventListener("load", addClickTracker);
