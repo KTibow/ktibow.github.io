e37b1316b61483055b81bbb466066c899ea59f39
kendell <KTibow@users.noreply.github.com>
2020-06-11 19:16:12 -0700

    Update gclass.js

--- gclass.js
+++ gclass.js
@@ -1,4 +1,5 @@
-if (!document.fonts.check("Open Sans")) {
+(function(d){function c(c){b.style.fontFamily=c;e.appendChild(b);f=b.clientWidth;e.removeChild(b);return f}var f,e=d.body,b=d.createElement("span");b.innerHTML=Array(100).join("wi");b.style.cssText=["position:absolute","width:auto","font-size:128px","left:-99999px"].join(" !important;");var g=c("monospace"),h=c("serif"),k=c("sans-serif"),X=c("Arial");window.isFontAvailable=function(b){return g!==c(b+",monospace")||k!==c(b+",sans-serif")||h!==c(b+",serif")||X!==c(b+",Arial")}})(document);
+if (!isFontAvailable("Open Sans")) {
 	var newFont = document.createElement('link');
 	newFont.rel = 'stylesheet';
 	newFont.href = 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap';
