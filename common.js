0a611036bbded0c974699417df01a2ecb557367a
kendell <KTibow@users.noreply.github.com>
2020-06-13 08:27:04 -0700

    Update common.js

--- common.js
+++ common.js
@@ -53,5 +53,22 @@ function loadDataStuff() {
   for (var i = 0; i < atags.length; i++) {
     atags[i].addEventListener("click", trackClick.bind({myelem: atags[i]}));
   }
+  function addLink() {
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br /><br /> Read more at: <a href='"+document.location.href+"'>"+document.location.href+"</a><br /> (&copy; Kendell R. You have permission to use, copy, publish, and distribute this with clear attribution, to the extent allowed by law.)";
+    var copytext = selection + pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+      body_element.removeChild(newdiv);
+    },0);
+  }
+  document.oncopy = addLink;
 }
 window.addEventListener("load", loadDataStuff);
