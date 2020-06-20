87fe48e8170961d51568eacade9331898b128c23
kendell <KTibow@users.noreply.github.com>
2020-06-20 06:59:39 -0700

    Update common.js

--- common.js
+++ common.js
@@ -36,6 +36,31 @@ function cookcheck() {
     document.getElementById("googwarn").style.setProperty("display", "none", "important");
   }
 }
+class ClassroomShare extends HTMLElement {
+	constructor() {
+		super();
+  }
+	connectedCallback() {
+		var currentPage = window.location;
+		var myAttributes = Array.from(this.attributes);
+		var mySharePage = "https://accounts.google.com/AccountChooser?continue=https%3A%2F%2Fclassroom.google.com%2Fshare%3Furl%3D"+currentPage;
+		var attr;
+		for (attr in myAttributes) {
+			mySharePage += "%26"+encodeURIComponent(myAttributes[attr].name)+"%3D"+encodeURIComponent(myAttributes[attr].value)
+		}
+		this.innerHTML = `<a href="${mySharePage}" style="text-decoration: none; display: inline-block; margin: 35px 10px 20px 32px; position: relative;">
+<img src="https://ktibow.github.io/classroom-logo.png" style="position: absolute; border-radius: 50%; height: 34px; top: -7px; left: -20px;" alt="">
+<span style="padding: 5px 8px 5px 24px; background-color: green; border-radius: 6px; color: white; font-family: Open Sans;">Share to Google Classroom</span>
+</a>`;
+		this.onmouseenter = function() {
+			this.style.filter = "brightness(92%)";
+		}
+		this.onmouseleave = function() {
+			this.style.filter = "brightness(100%)";
+		}
+	}
+}
+customElements.define("gclass-share", ClassroomShare);
 setTimeout(function() {
   var styl = document.createElement("style");
   styl.innerHTML = `.padlink {
