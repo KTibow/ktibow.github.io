2e22fd919013d7e60a8568500862df514a5fe3db
Kendell R <KTibow@users.noreply.github.com>
2021-01-19 13:29:44 -0800

    Make buttons fit and local themes work

--- card/theme.js
+++ card/theme.js
@@ -1,14 +1,24 @@
+function fixForLocal(url) {
+  if (window.location.protocol == "file:") {
+    if (window.location.href.includes("themes/index.html")) {
+       return url.replace("/card/themes/", "");
+    } else {
+       return url.replace("/card/", "");
+    }
+  }
+  return url;
+}
 /* main.css */
 var mainStyle = document.createElement("link");
 mainStyle.rel = "stylesheet";
-mainStyle.href = "/card/themes/main.css";
+mainStyle.href = fixForLocal("/card/themes/main.css");
 document.head.appendChild(mainStyle);
 /* Base styling */
 var baseStyle = document.createElement("link");
 baseStyle.rel = "stylesheet";
-baseStyle.href = "/card/themes/3d.css";
+baseStyle.href = fixForLocal("/card/themes/3d.css");
 if (localStorage.getItem("theme") !== null) {
-  baseStyle.href = `/card/themes/${localStorage.getItem("theme")}.css`;
+  baseStyle.href = fixForLocal(`/card/themes/${localStorage.getItem("theme")}.css`);
 }
 document.head.appendChild(baseStyle);
 /* Color styling */
@@ -16,15 +26,15 @@ var colorStyle = document.createElement("link");
 colorStyle.rel = "stylesheet";
 if (localStorage.getItem("dark") !== null) {
   if (localStorage.getItem("theme") !== null) {
-    colorStyle.href = `/card/themes/${localStorage.getItem("theme")}_${localStorage.getItem("dark")}.css`;
+    colorStyle.href = fixForLocal(`/card/themes/${localStorage.getItem("theme")}_${localStorage.getItem("dark")}.css`);
   } else {
-    colorStyle.href = `/card/themes/3d_${localStorage.getItem("dark")}.css`;
+    colorStyle.href = fixForLocal(`/card/themes/3d_${localStorage.getItem("dark")}.css`);
   }
 } else {
   if (localStorage.getItem("theme") !== null) {
-    colorStyle.href = `/card/themes/${localStorage.getItem("theme")}_light.css`;
+    colorStyle.href = fixForLocal(`/card/themes/${localStorage.getItem("theme")}_light.css`);
   } else {
-    colorStyle.href = `/card/themes/3d_light.css`;
+    colorStyle.href = fixForLocal(`/card/themes/3d_light.css`);
   }
 }
 document.head.appendChild(colorStyle);
