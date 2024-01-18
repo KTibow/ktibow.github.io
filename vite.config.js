5d118ce8e659872eb8e9bfc40635c0d1cd13e169
Kendell R <KTibow@users.noreply.github.com>
2024-01-18 08:28:10 -0800

    sitemap generator

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from "vite";
 import fg from "fast-glob";
+import { writeFile } from "fs/promises";
+import { defineConfig } from "vite";
 
 const html = await fg(["**/*.html", "!build/**/*", "!public/**/*"]);
+
+/** @type {import("vite").Plugin} */
+const sitemapPlugin = {
+  name: "sitemap-plugin",
+  async closeBundle() {
+    const blogs = await fg(["blog/**/*.html"]);
+    const pages = [...blogs]
+      .map(
+        (path) => "https://ktibow.github.io/" + path.replace(/index\.html$/, "")
+      )
+      .join("\n");
+    await writeFile("build/sitemap.txt", pages);
+  },
+};
+
 export default defineConfig({
   appType: "mpa",
   build: {
@@ -11,4 +27,5 @@ export default defineConfig({
       input: html,
     },
   },
+  plugins: [sitemapPlugin],
 });
