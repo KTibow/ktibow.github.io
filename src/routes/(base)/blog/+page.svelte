7a76a081bc452dff43ba746680652821cb0e2fee
KTibow <KTibow@users.noreply.github.com>
2025-04-02 20:20:01 -0700

    change blog index structure and add css injection

--- src/routes/(base)/blog/+page.svelte
+++ src/routes/(base)/blog/+page.svelte
@@ -1,55 +1,54 @@
-<script>
+<script lang="ts">
   import Ripple from "$lib/Ripple.svelte";
-
-  const blogPosts = [
-    { slug: "sleep", title: "How one estimates sleep cycles" },
-    { slug: "humanresearch", title: "Human Research" },
-    { slug: "interesting", title: "31% of 2FA codes are interesting" },
-    { slug: "gflash", title: "Why is Gemini Flash so popular?" },
-    { slug: "simplymatch", title: "Simply match curly brackets" },
-    { slug: "reasoningtax", title: "I invented a new term" },
-    { slug: "infidelity", title: "The Own Wife eval" },
-    { slug: "texihi", title: "TeXIHI" },
-    { slug: "rps", title: "The Rock Paper Scissors eval" },
-    { slug: "focussis", title: "What <em>is</em> Focus SIS" },
-    { slug: "nixcraft", title: "Why I don't like nixCraft" },
-    { slug: "zedthoughts", title: "What I think of Zed" },
-    { slug: "slidestemplates", title: "Slides Templates are funny" },
-    { slug: "selectingtext", title: "Selecting is easy, actually" },
-    { slug: "geminiaudio", title: "Gemini > Whisper????" },
-    { slug: "farewellcfpages", title: "What Do You Do When Cloudflare Pages Gets Blocked?" },
-    { slug: "aurora", title: "Unlinking Zigbee devices (Aurora)" },
-    { slug: "routellm", title: "Ripping off RouteLLM" },
-    { slug: "grading", title: "Grade math" },
-    { slug: "compressionstream", title: "A stream of thoughts on compression" },
-    { slug: "llama3", title: "Llama 3 fails some basic tests" },
-    { slug: "useesm", title: "Rant: Why aren't you using ESM (downranked)" },
-    { slug: "hafrontend", title: "My gripes with the HA frontend" },
-    { slug: "admaven", title: "We probably shouldn't use AdMaven" },
-    { slug: "chromasubsampling", title: "Why do we still use chroma subsampling" },
-    { slug: "floatprecision", title: "You don't need that big of a float precision" },
-    { slug: "toiconsets", title: "How I published 165 icon sets to NPM" },
-    { slug: "aipredictions", title: "2024 AI predictions" },
-    { slug: "improvements", title: "Life improvements I've done" },
-    { slug: "vcareexplanation", title: "VC31 explanation" },
-    { slug: "materialmenus", title: "Material menus are goofy" },
-    { slug: "isthisarat", title: "Is This A Rat has problems" },
-    { slug: "thebestai", title: "AI isn't good without good UI/UX" },
-    { slug: "2vFiomba", title: "2vFiomba" },
-  ];
 </script>
 
 <svelte:head>
   <title>@KTibow's blog</title>
 </svelte:head>
 
+{#snippet blog(page: string, title: string)}
+  <a href="/blog/{page}" class="entry">
+    <Ripple />
+    {@html title}
+  </a>
+{/snippet}
+
 <div class="flex flex-wrap flex-grow gap-2 p-6">
-  {#each blogPosts as post}
-    <a href="/blog/{post.slug}/" class="entry">
-      <Ripple />
-      {@html post.title}
-    </a>
-  {/each}
+  {@render blog("sleep/", "How one estimates sleep cycles")}
+  {@render blog("humanresearch/", "Human Research")}
+  {@render blog("cssinjection/work.pdf", "CSS Injection is All You Need")}
+  {@render blog("interesting/", "31% of 2FA codes are interesting")}
+  {@render blog("gflash/", "Why is Gemini Flash so popular?")}
+  {@render blog("simplymatch/", "Simply match curly brackets")}
+  {@render blog("reasoningtax/", "I invented a new term")}
+  {@render blog("infidelity/", "The Own Wife eval")}
+  {@render blog("texihi/", "TeXIHI")}
+  {@render blog("rps/", "The Rock Paper Scissors eval")}
+  {@render blog("focussis/", "What <em>is</em> Focus SIS")}
+  {@render blog("nixcraft/", "Why I don't like nixCraft")}
+  {@render blog("zedthoughts/", "What I think of Zed")}
+  {@render blog("slidestemplates/", "Slides Templates are funny")}
+  {@render blog("selectingtext/", "Selecting is easy, actually")}
+  {@render blog("geminiaudio/", "Gemini > Whisper????")}
+  {@render blog("farewellcfpages/", "What Do You Do When Cloudflare Pages Gets Blocked?")}
+  {@render blog("aurora/", "Unlinking Zigbee devices (Aurora)")}
+  {@render blog("routellm/", "Ripping off RouteLLM")}
+  {@render blog("grading/", "Grade math")}
+  {@render blog("compressionstream/", "A stream of thoughts on compression")}
+  {@render blog("llama3/", "Llama 3 fails some basic tests")}
+  {@render blog("useesm/", "Rant: Why aren't you using ESM (downranked)")}
+  {@render blog("hafrontend/", "My gripes with the HA frontend")}
+  {@render blog("admaven/", "We probably shouldn't use AdMaven")}
+  {@render blog("chromasubsampling/", "Why do we still use chroma subsampling")}
+  {@render blog("floatprecision/", "You don't need that big of a float precision")}
+  {@render blog("toiconsets/", "How I published 165 icon sets to NPM")}
+  {@render blog("aipredictions/", "2024 AI predictions")}
+  {@render blog("improvements/", "Life improvements I've done")}
+  {@render blog("vcareexplanation/", "VC31 explanation")}
+  {@render blog("materialmenus/", "Material menus are goofy")}
+  {@render blog("isthisarat/", "Is This A Rat has problems")}
+  {@render blog("thebestai/", "AI isn't good without good UI/UX")}
+  {@render blog("2vFiomba/", "2vFiomba")}
 </div>
 
 <style>
