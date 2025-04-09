e5dce35db53727e6f478a0690cd062becc8d4d67
KTibow <KTibow@users.noreply.github.com>
2025-04-09 08:59:05 -0700

    revise porkbunt

--- src/routes/blog/porkbunt/+page.svelte
+++ src/routes/blog/porkbunt/+page.svelte
@@ -11,8 +11,8 @@
   > about it. But today I learned they went so far as to make a phishing website.
 </p>
 <p>
-  I'm talking about the one and only PorkBunt.com, designed to trick clients of the domain registrar
-  PorkBun. I checked, and anyone who makes one of these typos will be phished:
+  I'm talking about the one and only Porkbunt.com, designed to trick clients of the domain registrar
+  Porkbun. I checked, and anyone who makes one of these typos will be phished:
 </p>
 <ul>
   <li>porknun.com</li>
@@ -26,4 +26,10 @@
 
 <p>
   <em>Inspiration: <a href="https://x.com/thdxr/status/1909657328099426496">@thdxr tweet</a></em>
+</p>
+
+<p>
+  <em>An alternate perspective:</em> While this is decieving, it's incorrect to describe it as
+  phishing, as it has you create an account separate from Porkbun. It's just independently ran
+  (ab)use of GoDaddy's <a href="https://www.godaddy.com/reseller-program">white label</a> program.
 </p>
