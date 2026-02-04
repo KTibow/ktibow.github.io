dd9b126762358f7cde6bee62ec77c6e6b49a2b06
KTibow <KTibow@users.noreply.github.com>
2026-02-04 14:46:07 -0800

    update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,6 +3,10 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = ({ url }) => {
   const sitemapURL = new URL("/sitemap-index.xml", url).href;
   return new Response(`# hi robots!
+# you can crawl basically everything on this website!
+
+User-agent: *
+Disallow:
 
 Sitemap: ${sitemapURL}
 `);
