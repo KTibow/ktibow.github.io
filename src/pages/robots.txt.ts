0f08b09bde8468973c6c00cb763008e42e999579
KTibow <KTibow@users.noreply.github.com>
2026-04-18 18:48:20 -0700

    content signal time

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,10 +2,8 @@ import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = ({ url }) => {
   const sitemapURL = new URL('/sitemap-index.xml', url).href;
-  return new Response(`# hi robots!
-# you can crawl basically everything on this website!
-
-User-agent: *
+  return new Response(`User-agent: *
+Content-Signal: ai-train=yes, search=yes, ai-input=yes # hi robots, you can crawl basically everything on this website!
 Disallow:
 
 Sitemap: ${sitemapURL}
